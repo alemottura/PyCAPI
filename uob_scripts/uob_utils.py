ded2785a3e37f3e73c3996bf6acd4a9837d51f41
alemottura <me@mottura.org>
2017-06-30 21:50:44 +0100

    Renaming of script

--- uob_scripts/uob_utils.py
+++ uob_scripts/uob_utils.py
@@ -1,3 +1,42 @@
+#
+#	uob_utils.py
+#
+#	These is a module that is required in some of the scripts provided.
+#	It was created to provide some classes and functions to lighten various
+#	scripts contained in Canvas.
+#
+#	First are two classes to simplifying emailing across all scripts. This
+#	way, details of SMTP server and credentials only need to be changed in
+#	one place for things to work.
+#
+#	Second are a series of functions that provide a number of useful date
+#	operations which are specifically designed for the University of
+#	Birmingham. The University uses a system of 'University' weeks to
+#	calendarise everything from academic terms to open days. The first
+#	'University' week (Week 1) is the week that contains the last Friday
+#	of August. The relationship between 'University' weeks and 'Term' weeks
+#	is fixed. Therefore, these function can find the relationship between
+#	any date and the University calendar.
+#
+#	Things that need to be set:
+#
+#	smtp server and login credentials
+smtp_server = 'smtp.gmail.com' # smtp server
+smtp_port = 465 # port number (ssl only, no insecure connections)
+smtp_username = None # username (if this is left as None, make sure you set up ~/.mailcredentials)
+smtp_password = None # password (if this is left as None, make sure you set up ~/.mailcredentials)
+smtp_from_addr = 'Alessandro Mottura <alemottura@gmail.com>' # From address for all emails
+#
+#	SMTP server authentication is normally done using a username and a password.
+#	If you do not wish to write you username and password above, you can write
+#	these into a simple text file with name '.mailcredentials' and save the file
+#	in your home directory. Make sure the file has restricted permissions (600).
+#
+#
+#
+#
+#
+#
 import calendar
 import datetime
 import math
@@ -11,7 +50,7 @@ from email.utils import formatdate
 
 class MailAPI():
 	""""""
-	def __init__(self, username=None, password=None, server='smtp.gmail.com', port=465, path_to_credentials='~/.mailcredentials'):
+	def __init__(self, username=smtp_username, password=smtp_password, server=smtp_server, port=smtp_port, path_to_credentials='~/.mailcredentials', from_addr=smtp_from_addr):
 		if username == None or password==None: # if username and password are not specified, look for .mailcredentials file
 			if not os.path.isfile(os.path.expanduser(path_to_credentials)):
 				raise RuntimeError('Provide a username and password in ~/.mailcredentials or as an argument of the call.')
@@ -28,8 +67,8 @@ class MailAPI():
 		except:
 			raise RuntimeError('Could not connect to e-mail server.')
 		
-	def send(self, from_addr, to_addr, msg):
-		return self.ssl.sendmail(from_addr, to_addr, msg.as_string())
+	def send(self, to_addr, msg):
+		return self.ssl.sendmail(self.from_addr, to_addr, msg.as_string())
 
 
 class EMailMessage(MIMEMultipart):
