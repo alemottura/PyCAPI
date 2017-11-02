f3f2f1c8b2af6093f2b55af7cb2edeb2219f3a53
Alessandro Mottura <me@mottura.org>
2017-11-02 09:19:03 +0000

    Email reminders to assignment status

--- uob_scripts/course_assignment_summary.py
+++ uob_scripts/course_assignment_summary.py
@@ -29,9 +29,9 @@ offset_days = 14
 #
 #       reminder_dates - days from due date for reminder emails to be sent
 reminder_dates = [
-    [14], # day to email teachers
-    [20] # day to email teachers and TSO
-    ]
+	[10], # day to email teachers
+	[15] # day to email teachers and TSO
+	]
 #
 #
 #       TSO_email - email of TSO
@@ -51,6 +51,7 @@ from openpyxl import Workbook
 from openpyxl.styles import colors
 from openpyxl.styles import Font, Color, PatternFill
 from openpyxl import worksheet
+import re
 
 
 capi = PyCAPI.CanvasAPI()
@@ -116,25 +117,25 @@ ws.column_dimensions[col_senroll].width = 20
 allcourses = capi.get_courses(account_id='114', include=['term', 'teachers', 'total_students'])
 courses = []
 for course in allcourses:
-    if course['term']['name'] in included_terms:
-        courses.append(course)
+	if course['term']['name'] in included_terms:
+		courses.append(course)
 #print json.dumps(courses, indent = 2)
 
 
 i = 2
 for course in courses: # Loop through all courses in Canvas account
-    ws[col_term+str(i)] = course['term']['name']
-    ws[col_cid+str(i)] = '=HYPERLINK("http://147.188.152.33:8080/eval.php?url=https://canvas.bham.ac.uk/courses/'+str(course['id'])+'", "'+str(course['id'])+'")'
-    ws[col_cnm+str(i)] = '=HYPERLINK("http://147.188.152.33:8080/eval.php?url=https://canvas.bham.ac.uk/courses/'+str(course['id'])+'", "'+str(course['name'])+'")'
-    ws[col_ccd+str(i)] = '=HYPERLINK("http://147.188.152.33:8080/eval.php?url=https://canvas.bham.ac.uk/courses/'+str(course['id'])+'", "'+str(course['course_code'])+'")'
-    ws[col_cadmin+str(i)] = course['account_id']
-    ws[col_cavail+str(i)] = course['workflow_state'].title()
-    if course['workflow_state'] != 'available':
-        for cell in ws[str(i)+':'+str(i)]:
-            cell.font = Font(color='00D3D3D3')
-    ws[col_tenroll+str(i)] = len(course['teachers'])
-    ws[col_senroll+str(i)] = course['total_students']
-    i += 1
+	ws[col_term+str(i)] = course['term']['name']
+	ws[col_cid+str(i)] = '=HYPERLINK("http://147.188.152.33:8080/eval.php?url=https://canvas.bham.ac.uk/courses/'+str(course['id'])+'", "'+str(course['id'])+'")'
+	ws[col_cnm+str(i)] = '=HYPERLINK("http://147.188.152.33:8080/eval.php?url=https://canvas.bham.ac.uk/courses/'+str(course['id'])+'", "'+str(course['name'])+'")'
+	ws[col_ccd+str(i)] = '=HYPERLINK("http://147.188.152.33:8080/eval.php?url=https://canvas.bham.ac.uk/courses/'+str(course['id'])+'", "'+str(course['course_code'])+'")'
+	ws[col_cadmin+str(i)] = course['account_id']
+	ws[col_cavail+str(i)] = course['workflow_state'].title()
+	if course['workflow_state'] != 'available':
+		for cell in ws[str(i)+':'+str(i)]:
+			cell.font = Font(color='00D3D3D3')
+	ws[col_tenroll+str(i)] = len(course['teachers'])
+	ws[col_senroll+str(i)] = course['total_students']
+	i += 1
 
 # Make the workbook into a filtered table.
 ws.auto_filter.ref = 'A1:AA'+str(i)
@@ -227,147 +228,152 @@ ws.column_dimensions[col_weight].width = 12
 #
 i = 2
 for course in courses:
-    course_assignments = capi.get_assignments(course['id'])
-    payload = {}
-    payload['include[]'] = ['assignments']
-    assignment_groups = capi.get('/courses/%s/assignment_groups' % course['id'], payload=payload)
-    analytics_missing = False
-    try:
-        analytics = capi.get('/courses/%s/analytics/assignments' % course['id'])
-    except:
-        analytics_missing = True
-    #print json.dumps(course_assignments, indent = 2)
-    #print json.dumps(assignment_groups, indent = 2)
-    for assignment in course_assignments:
-        ws[col_cid+str(i)] = '=HYPERLINK("http://147.188.152.33:8080/eval.php?url=https://canvas.bham.ac.uk/courses/'+str(course['id'])+'", "'+str(course['id'])+'")'
-        ws[col_cnm+str(i)] = '=HYPERLINK("http://147.188.152.33:8080/eval.php?url=https://canvas.bham.ac.uk/courses/'+str(course['id'])+'", "'+str(course['name'])+'")'
-        ws[col_ccd+str(i)] = '=HYPERLINK("http://147.188.152.33:8080/eval.php?url=https://canvas.bham.ac.uk/courses/'+str(course['id'])+'", "'+str(course['course_code'])+'")'  
-        ws[col_term+str(i)] = course['term']['name']
-        ws[col_asgnid+str(i)] = '=HYPERLINK("http://147.188.152.33:8080/eval.php?url=https://canvas.bham.ac.uk/courses/'+str(course['id'])+'/assignments/'+str(assignment['id'])+'", "'+str(assignment['id'])+'")'
-        ws[col_asgnnm+str(i)] = '=HYPERLINK("http://147.188.152.33:8080/eval.php?url=https://canvas.bham.ac.uk/courses/'+str(course['id'])+'/assignments/'+str(assignment['id'])+'", "'+str(assignment['name'].encode('ascii', 'ignore'))+'")'
-        ws[col_cadmin+str(i)] = course['account_id']
-        ws[col_cavail+str(i)]= course['workflow_state'].title()
-        if assignment['published']:
-            ws[col_asgnavail+str(i)] = 'Published'
-        else:
-            ws[col_asgnavail+str(i)] = 'Unpublished'
-        if 'online_upload' in assignment['submission_types'] or 'online_quiz' in assignment['submission_types']:
-            ws[col_asgntype+str(i)] = 'Online'
-        else:
-            ws[col_asgntype+str(i)] = 'Paper'
-        if assignment['group_category_id'] == None:
-            ws[col_group+str(i)] = 'No'
-        else:
-            ws[col_group+str(i)] = 'Yes'
-        if assignment['unlock_at'] == None:
-            ws[col_unlock+str(i)] = 'None set'
-        else:
-            ws[col_unlock+str(i)] = str(datetime.datetime.strptime(assignment['unlock_at'], "%Y-%m-%dT%H:%M:%SZ"))
-        if assignment['due_at'] == None:
-            ws[col_due+str(i)] = 'None set'
-            ws[col_grby+str(i)] = 'None set'
-        else:
-            due_date = datetime.datetime.strptime(assignment['due_at'], "%Y-%m-%dT%H:%M:%SZ")
-            ws[col_due+str(i)] = str(due_date)
-            grade_by = ((pandas.Timestamp(np.busday_offset(due_date, offset_days, roll='forward'))).to_pydatetime()).date()
-            ws[col_grby+str(i)] = str(grade_by)
-            recipients = []
-            cc_recipients = []
-            if (int(np.busday_count(due_date,today)) in reminder_dates[0]) and assignment['needs_grading_count'] != 0: # If reminder email for teacher is needed
-                # Collects email of all teachers for assignment to email
-                for course_teacher in course['teachers']:
-                    teacher = capi.get_user(course_teacher['id'])
-                    recipients.append(teacher['primary_email'])
-                message_subject = '*CANVAS TESTING* IMPORTANT: There are assignments ungraded'
-                message_body = """Hi,
+	course_assignments = capi.get_assignments(course['id'])
+	payload = {}
+	payload['include[]'] = ['assignments']
+	assignment_groups = capi.get('/courses/%s/assignment_groups' % course['id'], payload=payload)
+	analytics_missing = False
+	try:
+		analytics = capi.get('/courses/%s/analytics/assignments' % course['id'])
+	except:
+		analytics_missing = True
+	#print json.dumps(course_assignments, indent = 2)
+	#print json.dumps(assignment_groups, indent = 2)
+	for assignment in course_assignments:
+		ws[col_cid+str(i)] = '=HYPERLINK("http://147.188.152.33:8080/eval.php?url=https://canvas.bham.ac.uk/courses/'+str(course['id'])+'", "'+str(course['id'])+'")'
+		ws[col_cnm+str(i)] = '=HYPERLINK("http://147.188.152.33:8080/eval.php?url=https://canvas.bham.ac.uk/courses/'+str(course['id'])+'", "'+str(course['name'])+'")'
+		ws[col_ccd+str(i)] = '=HYPERLINK("http://147.188.152.33:8080/eval.php?url=https://canvas.bham.ac.uk/courses/'+str(course['id'])+'", "'+str(course['course_code'])+'")'  
+		ws[col_term+str(i)] = course['term']['name']
+		ws[col_asgnid+str(i)] = '=HYPERLINK("http://147.188.152.33:8080/eval.php?url=https://canvas.bham.ac.uk/courses/'+str(course['id'])+'/assignments/'+str(assignment['id'])+'", "'+str(assignment['id'])+'")'
+		ws[col_asgnnm+str(i)] = '=HYPERLINK("http://147.188.152.33:8080/eval.php?url=https://canvas.bham.ac.uk/courses/'+str(course['id'])+'/assignments/'+str(assignment['id'])+'", "'+str(assignment['name'].encode('ascii', 'ignore'))+'")'
+		ws[col_cadmin+str(i)] = course['account_id']
+		ws[col_cavail+str(i)]= course['workflow_state'].title()
+		if assignment['published']:
+			ws[col_asgnavail+str(i)] = 'Published'
+		else:
+			ws[col_asgnavail+str(i)] = 'Unpublished'
+		if 'online_upload' in assignment['submission_types'] or 'online_quiz' in assignment['submission_types']:
+			ws[col_asgntype+str(i)] = 'Online'
+		else:
+			ws[col_asgntype+str(i)] = 'Paper'
+		if assignment['group_category_id'] == None:
+			ws[col_group+str(i)] = 'No'
+		else:
+			ws[col_group+str(i)] = 'Yes'
+		if assignment['unlock_at'] == None:
+			ws[col_unlock+str(i)] = 'None set'
+		else:
+			ws[col_unlock+str(i)] = str(datetime.datetime.strptime(assignment['unlock_at'], "%Y-%m-%dT%H:%M:%SZ"))
+		if assignment['due_at'] == None:
+			ws[col_due+str(i)] = 'None set'
+			ws[col_grby+str(i)] = 'None set'
+		else:
+			due_date = datetime.datetime.strptime(assignment['due_at'], "%Y-%m-%dT%H:%M:%SZ")
+			ws[col_due+str(i)] = str(due_date)
+			grade_by = ((pandas.Timestamp(np.busday_offset(due_date, offset_days, roll='forward'))).to_pydatetime()).date()
+			ws[col_grby+str(i)] = str(grade_by)
+			#recipients = []
+			#cc_recipients = []
+			if (int(np.busday_count(due_date,today)) in reminder_dates[0]) and assignment['needs_grading_count'] != 0: # If reminder email for teacher is needed
+				# Collects email of all teachers for assignment to email
+				recipients = re.findall('[A-Za-z0-9\.]*@bham.ac.uk',assignment['description'])
+				if len(recipients) > 0:
+				#for course_teacher in course['teachers']:
+				#	teacher = capi.get_user(course_teacher['id'])
+				#	recipients.append(teacher['primary_email'])
+					message_subject = 'IMPORTANT: An assignment needs to be graded'
+					message_body = """Hi,
 
-This is an IMPORTANT reminder that the due date for grading the assignment: %s has been reached and there are still submissions left ungraded.
-Please can this be addressed as soon as possible.
+This is an IMPORTANT reminder that the due date for grading %s will be in 5 days and there are still submissions left ungraded.
+Could you please address this as soon as possible?
 Link: %s
 
 Best Regards,
 Met&Mat Office
-			""" % (assignment['name'], assignment['html_url'])
-                # Now send email to teachers
-                msg = uob_utils.EMailMessage(", ".join(recipients), message_subject)
-                msg.body(message_body)
-                all_recipients = recipients + cc_recipients
-                #mail.send(all_recipients, msg) # Send email
-            elif (int(np.busday_count(due_date,today)) in reminder_dates[1]) and assignment['needs_grading_count'] != 0: # If reminder email for teacher and TSO is needed
-                # Collects email of all teachers for assignment to email
-                for course_teacher in course['teachers']:
-                    teacher = capi.get_user(course_teacher['id'])
-                    recipients.append(teacher['primary_email'])
-                cc_recipients.extend(TSO_email)
-                message_subject = '*CANVAS TESTING* ATTENTION REQUIRED: Assignment submissions still ungraded'
-                message_body = """Hi,
+					""" % (assignment['name'], assignment['html_url'])
+					# Now send email to teachers
+					msg = uob_utils.EMailMessage(", ".join(recipients), message_subject)
+					msg.body(message_body)
+					#all_recipients = recipients + cc_recipients
+					mail.send(recipients, msg) # Send email
+			elif (int(np.busday_count(due_date,today)) in reminder_dates[1]) and assignment['needs_grading_count'] != 0: # If reminder email for teacher and TSO is needed
+				# Collects email of all teachers for assignment to email
+				recipients = TSO_email
+				recipients.extend(re.findall('[A-Za-z0-9\.]*@bham.ac.uk',assignment['description']))
+				if len(recipients) > 0:
+				#for course_teacher in course['teachers']:
+				#	teacher = capi.get_user(course_teacher['id'])
+				#	recipients.append(teacher['primary_email'])
+				#cc_recipients.extend(TSO_email)
+					message_subject = 'ATTENTION REQUIRED: Assignment submissions still ungraded'
+					message_body = """Hi,
 
-This is an VERY IMPORTANT reminder that the due date for grading the assignment: %s has been passed and there are still submissions left ungraded.
+This is an VERY IMPORTANT reminder that the due date for grading %s is today, and there are still submissions left ungraded.
 This must be addressed immediately.
 Link: %s
 
 Best Regards,
 Met&Mat Office
-			""" % (assignment['name'], assignment['html_url'])
-                # Now send email to teachers and TSO
-                msg = uob_utils.EMailMessage(", ".join(recipients), message_subject, cc_addr=", ".join(cc_recipients))
-                msg.body(message_body)
-                all_recipients = recipients + cc_recipients
-                #mail.send(all_recipients, msg) # Send email
-        if assignment['lock_at'] == None:
-            ws[col_lock+str(i)] = 'None set'
-        else:
-            ws[col_lock+str(i)] = str(datetime.datetime.strptime(assignment['lock_at'], "%Y-%m-%dT%H:%M:%SZ"))
-        #ws[col_sub+str(i)] =
-        ws[col_ungr+str(i)] = assignment['needs_grading_count']
-        if analytics_missing == False:
-            for analytic in analytics:
-                if analytic['assignment_id'] == assignment['id']:
-                    if analytic['tardiness_breakdown']['missing'] == None:
-                        ws[col_miss+str(i)] = 'None'
-                    else:
-                        ws[col_miss+str(i)] = analytic['tardiness_breakdown']['missing']
-                    if analytic['tardiness_breakdown']['late'] == None:
-                        ws[col_late+str(i)] = 'None'
-                    else:
-                        ws[col_late+str(i)] = analytic['tardiness_breakdown']['late']
-                    if analytic['median'] == None:
-                        ws[col_median+str(i)] = 'None'
-                    else:
-                        ws[col_median+str(i)] = analytic['median']
-        else:
-            ws[col_miss+str(i)] = 'Missing'
-            ws[col_late+str(i)] = 'Missing'
-            ws[col_median+str(i)] = 'Missing'
-        if assignment['omit_from_final_grade'] == False:
-            ws[col_fingr+str(i)] = 'Yes'
-        else:
-            ws[col_fingr+str(i)] = 'No'
-        for assignment_group in assignment_groups:
-            for assignment_group_assignment in assignment_group['assignments']:
-                if assignment_group_assignment['id'] == assignment['id']:
-                    ws[col_weight+str(i)] = assignment_group['group_weight']/len(assignment_group['assignments'])
-        if assignment['muted'] == False:
-            ws[col_muted+str(i)] = 'No'
-        else:
-            ws[col_muted+str(i)] = 'Yes'
-        
-        # Colour the relevant lines
-        if ws[col_asgntype+str(i)].value == 'Paper':
-            for cell in ws[str(i)+':'+str(i)]:
-                cell.font = Font(color='FF0000')
-        if assignment['due_at'] != None:
-            if datetime.datetime.strptime(assignment['due_at'], "%Y-%m-%dT%H:%M:%SZ") + datetime.timedelta(days=14) < today and assignment['needs_grading_count'] > 0:
-                for cell in ws[str(i)+':'+str(i)]:
-                    cell.fill = PatternFill(fill_type='solid', start_color='FFFF00', end_color='FFFF00')
-            if datetime.datetime.strptime(assignment['due_at'], "%Y-%m-%dT%H:%M:%SZ") < today and assignment['needs_grading_count'] == 0 and assignment['muted'] == False and ws[col_asgntype+str(i)].value != 'Paper':
-                for cell in ws[str(i)+':'+str(i)]:
-                    cell.font = Font(color='00FF00')
-        if course['workflow_state'] != 'available' or not assignment['published']:
-            for cell in ws[str(i)+':'+str(i)]:
-                cell.font = Font(color='00D3D3D3')
-        
-        i += 1
+					""" % (assignment['name'], assignment['html_url'])
+					# Now send email to teachers and TSO
+					msg = uob_utils.EMailMessage(", ".join(recipients), message_subject, cc_addr=", ".join(cc_recipients))
+					msg.body(message_body)
+					#all_recipients = recipients + cc_recipients
+					mail.send(recipients, msg) # Send email
+		if assignment['lock_at'] == None:
+			ws[col_lock+str(i)] = 'None set'
+		else:
+			ws[col_lock+str(i)] = str(datetime.datetime.strptime(assignment['lock_at'], "%Y-%m-%dT%H:%M:%SZ"))
+		#ws[col_sub+str(i)] =
+		ws[col_ungr+str(i)] = assignment['needs_grading_count']
+		if analytics_missing == False:
+			for analytic in analytics:
+				if analytic['assignment_id'] == assignment['id']:
+					if analytic['tardiness_breakdown']['missing'] == None:
+						ws[col_miss+str(i)] = 'None'
+					else:
+						ws[col_miss+str(i)] = analytic['tardiness_breakdown']['missing']
+					if analytic['tardiness_breakdown']['late'] == None:
+						ws[col_late+str(i)] = 'None'
+					else:
+						ws[col_late+str(i)] = analytic['tardiness_breakdown']['late']
+					if analytic['median'] == None:
+						ws[col_median+str(i)] = 'None'
+					else:
+						ws[col_median+str(i)] = analytic['median']
+		else:
+			ws[col_miss+str(i)] = 'Missing'
+			ws[col_late+str(i)] = 'Missing'
+			ws[col_median+str(i)] = 'Missing'
+		if assignment['omit_from_final_grade'] == False:
+			ws[col_fingr+str(i)] = 'Yes'
+		else:
+			ws[col_fingr+str(i)] = 'No'
+		for assignment_group in assignment_groups:
+			for assignment_group_assignment in assignment_group['assignments']:
+				if assignment_group_assignment['id'] == assignment['id']:
+					ws[col_weight+str(i)] = assignment_group['group_weight']/len(assignment_group['assignments'])
+		if assignment['muted'] == False:
+			ws[col_muted+str(i)] = 'No'
+		else:
+			ws[col_muted+str(i)] = 'Yes'
+		
+		# Colour the relevant lines
+		if ws[col_asgntype+str(i)].value == 'Paper':
+			for cell in ws[str(i)+':'+str(i)]:
+				cell.font = Font(color='FF0000')
+		if assignment['due_at'] != None:
+			if datetime.datetime.strptime(assignment['due_at'], "%Y-%m-%dT%H:%M:%SZ") + datetime.timedelta(days=14) < today and assignment['needs_grading_count'] > 0:
+				for cell in ws[str(i)+':'+str(i)]:
+					cell.fill = PatternFill(fill_type='solid', start_color='FFFF00', end_color='FFFF00')
+			if datetime.datetime.strptime(assignment['due_at'], "%Y-%m-%dT%H:%M:%SZ") < today and assignment['needs_grading_count'] == 0 and assignment['muted'] == False and ws[col_asgntype+str(i)].value != 'Paper':
+				for cell in ws[str(i)+':'+str(i)]:
+					cell.font = Font(color='00FF00')
+		if course['workflow_state'] != 'available' or not assignment['published']:
+			for cell in ws[str(i)+':'+str(i)]:
+				cell.font = Font(color='00D3D3D3')
+		
+		i += 1
 
 # Make the workbook into a filtered table.
 ws.auto_filter.ref = 'A1:AA'+str(i)
